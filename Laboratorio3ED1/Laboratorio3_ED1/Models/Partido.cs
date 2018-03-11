168a6b29ea0331b75585fe81b01f3019ee962910
Angel Jimenez <angeljpp29@gmail.com>
2018-03-11 16:12:19 -0600

    Clase Partido creada

---
+++ Laboratorio3ED1/Laboratorio3_ED1/Models/Partido.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio3_ED1.Models
+{
+    public class Partido : IComparable
+    {
+        public int NoPartido { get; set; }
+        public DateTime FechaPartido { get; set; }
+        public string Grupo { get; set; }
+        public string Pais1 { get; set; }
+        public string Pais2 { get; set; }
+        public string Estadio { get; set; }
+
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
